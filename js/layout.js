e800f2debc02502dcbd3efbd695c337c076c0b88
ngoc1397 <73026491+ngoc1397@users.noreply.github.com>
2021-10-20 10:46:42 +0700

    init

---
+++ js/layout.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('#btn-open-menu').click(function() {
+        $('.menu-mobile').addClass('show-menu')
+    })
+    $('#btn-close-menu').click(function() {
+        $('.menu-mobile').removeClass('show-menu')
+    })
+    $('#btn-talk').click(function() {
+        $('#get-talk').addClass('show-talk')
+    })
+    $('#btn-close-talk').click(function() {
+        $('#get-talk').removeClass('show-talk')
+    })
+    $('#btn-filter').click(function() {
+        $('#popup-for-filter').toggleClass('show-popup')
+        $('#popup-for-provider').removeClass('show-popup')
+    })
+    $('#btn-provider').click(function() {
+        $('#popup-for-provider').toggleClass('show-popup')
+        $('#popup-for-filter').removeClass('show-popup')
+    })
+})
